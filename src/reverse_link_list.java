5db390ff4adcb023b7f99de8b4b715ce85b8a78e
Ayushi Gupta <ayushiguota4050@gmail.com>
2025-10-04 05:01:51 +0530

    added list

--- src/reverse_link_list.java
+++ src/reverse_link_list.java
@@ -5,6 +5,8 @@ public class reverse_link_list {
          Assume that n is the list's length, the time complexity is O(n).
 
          Space complexity : O(1).
+         Given list= [1,2,3,4,5]
+         Output: [5,4,3,2,1]
          */
         ListNode prev = null;
         ListNode curr = head;
