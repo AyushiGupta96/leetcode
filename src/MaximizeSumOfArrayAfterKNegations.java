c8a0057f4e087981c971633cc26dac884151c7a7
Ayushi Gupta <ayushiguota4050@gmail.com>
2025-11-25 11:16:02 -0800

    1005. Maximize Sum Of Array After K Negations

---
+++ src/MaximizeSumOfArrayAfterKNegations.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class MaximizeSumOfArrayAfterKNegations {
+    public int largestSumAfterKNegations(int[] nums, int k) {
+        for(int i=0;i<k;i++){
+            Arrays.sort(nums);
+            nums[0] = -nums[0];
+        }
+        int sum = 0;
+        for(int num : nums){
+            sum += num;
+        }
+        return sum;
+    }
+}
