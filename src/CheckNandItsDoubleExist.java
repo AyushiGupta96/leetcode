c3b1dc49721ea36e8dedd45ceed16f73bfaf9947
Ayushi Gupta <ayushiguota4050@gmail.com>
2026-01-03 11:49:56 -0800

    1346. Check If N and Its Double Exist

---
+++ src/CheckNandItsDoubleExist.java
@@ -0,0 +1,33 @@
+public class CheckNandItsDoubleExist {
+    /** Given an array arr of integers, check if there exist two indices i and j such that :
+
+     i != j
+     0 <= i, j < arr.length
+     arr[i] == 2 * arr[j]
+
+
+     Example 1:
+
+     Input: arr = [10,2,5,3]
+     Output: true
+     Explanation: For i = 0 and j = 2, arr[i] == 10 == 2 * 5 == 2 * arr[j]
+     Example 2:
+
+     Input: arr = [3,1,7,11]
+     Output: false
+     Explanation: There is no i and j that satisfy the conditions.
+*/
+    public boolean checkIfExist(int[] arr) {
+         //step 1: Iterate through all pairs of indices
+for (int i = 0; i < arr.length; i++) {
+for (int j = 0; j < arr.length; j++) {
+           //check the condition
+if (i != j && arr[i] == 2 * arr[j]) {
+return true;
+}
+}
+}
+       //no valid pait found
+return false;
+}
+     }
