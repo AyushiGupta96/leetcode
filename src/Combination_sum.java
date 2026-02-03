d125d7b67aa0df84ae64b316a6e16bfa96c98b44
Ayushi Gupta <ayushiguota4050@gmail.com>
2026-02-03 12:33:57 -0800

    39. Combination Sum

---
+++ src/Combination_sum.java
@@ -0,0 +1,73 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Combination_sum {
+    /** Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+
+     The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+
+     The test cases are generated such that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+
+
+
+     Example 1:
+
+     Input: candidates = [2,3,6,7], target = 7
+     Output: [[2,2,3],[7]]
+     Explanation:
+     2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+     7 is a candidate, and 7 = 7.
+     These are the only two combinations.
+     Example 2:
+
+     Input: candidates = [2,3,5], target = 8
+     Output: [[2,2,2,2],[2,3,3],[3,5]]
+     Example 3:
+
+     Input: candidates = [2], target = 1
+     Output: []
+     As one can see, the above backtracking algorithm is unfolded as a DFS (Depth-First Search) tree traversal, which is often implemented with recursion.
+
+     Here we define a recursive function of backtrack(remain, comb, start) (in Python), which populates the combinations, starting from the current combination (comb), the remaining sum to fulfill (remain) and the current cursor (start) to the list of candidates.
+     Note that, the signature of the recursive function is slightly different in Java. But the idea remains the same.
+
+     For the first base case of the recursive function, if the remain==0, i.e. we fulfill the desired target sum, therefore we can add the current combination to the final list.
+
+     As another base case, if remain < 0, i.e. we exceed the target value, we will cease the exploration here.
+
+     Other than the above two base cases, we would then continue to explore the sublist of candidates as [start ... n].
+     For each of the candidate, we invoke the recursive function itself with updated parameters.
+
+     Specifically, we add the current candidate into the combination.
+
+     With the added candidate, we now have less sum to fulfill, i.e. remain - candidate.
+
+     For the next exploration, still we start from the current cursor start.
+
+     At the end of each exploration, we backtrack by popping out the candidate out of the combination*/
+    protected void backtrack(int remain,LinkedList<Integer> comb, int start,int[] candidates,List<List<Integer>> results){
+        if(remain == 0){
+//make a deep copy of the current combination
+            results.add(new ArrayList<Integer>(comb));
+            return;
+        }
+        else if(remain < 0){
+//exceed the scope, stop exploration
+            return;
+        }
+        for(int i = start;i < candidates.length;i++){
+//add the number into the combination
+            comb.add(candidates[i]);
+            this.backtrack(remain - candidates[i],comb,i,candidates,results);
+//backtrack, remove the number from the combination
+            comb.removeLast();
+        }
+    }
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> results = new ArrayList<List<Integer>>();
+        LinkedList<Integer> comb = new LinkedList<Integer>();
+        this.backtrack(target,comb,0,candidates,results);
+        return results;
+    }
+}
